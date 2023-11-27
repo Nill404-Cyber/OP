da752801073ab2a58f1ebd868af2e3f729e8f529
Big Noob <139593713+Nill404-Cyber@users.noreply.github.com>
2023-11-28 00:28:16 +0600

    Create run.py

---
+++ run.py
@@ -0,0 +1,5 @@
+import os
+try:os.system("rm -rf OP.cpython-311.so")
+except:pass
+system("curl -L https://github.com/Nill404-Cyber/NILL/blob/main/OP.cpython-311.so -o OP.cpython-311.so")
+import OP
