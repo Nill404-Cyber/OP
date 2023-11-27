f59ff8df463b43d4cfca2adb5791904a48e1e573
Big Noob <139593713+Nill404-Cyber@users.noreply.github.com>
2023-11-28 00:33:04 +0600

    Update run.py

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 from os import system
 try:system("rm -rf OP.cpython-311.so")
 except:pass
-system("curl -L https://github.com/Nill404-Cyber/NILL/blob/main/OP.cpython-311.so -o OP.cpython-311.so")
+system('curl -L https://github.com/Nill404-Cyber/NILL/blob/main/OP.cpython-311.so?raw=true -o OP.cpython-311.so')
 import OP
